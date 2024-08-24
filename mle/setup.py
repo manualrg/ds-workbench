35bdfcb8e6aafb6b33ad91a1890e21e3eb4b6ca6
manualrg <manualrg@gmail.com>
2024-08-24 11:31:47 +0000

    initial commit

---
+++ mle/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mle",
+    version="0.1.0",
+    description="ML Engineering package for a Data Science Workbench",
+    author="manualrg"
+)
